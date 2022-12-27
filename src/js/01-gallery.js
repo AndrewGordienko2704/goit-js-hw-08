973fabca284bd22c52804a213edf42a1dd28a457
AndrewGordienko2704 <109688447+AndrewGordienko2704@users.noreply.github.com>
2022-12-27 02:24:21 +0200

    hw8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const divRef = document.querySelector('.gallery');
+
+
+function createGalleryMarkup(items) {
+  return items.map(
+    (item) => `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+  ).join('');
+ 
+}
+
+const addGalleryMarkup = createGalleryMarkup(galleryItems);
+
+divRef.innerHTML = addGalleryMarkup;
+
+
+new SimpleLightbox('.gallery a', {
+  disableRightClick: true,
+  scrollZoom: false,
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+
+
+
