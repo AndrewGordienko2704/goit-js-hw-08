f650a04b1e557d05e7bd60afe3ab20c8e28f9737
AndrewGordienko2704 <109688447+AndrewGordienko2704@users.noreply.github.com>
2023-01-04 23:57:29 +0200

    hw_08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,57 +1,123 @@
-// Import library
 import throttle from 'lodash.throttle';
-import * as storageLocal from './storage';
 
-// Initial constants
-const FB_FORM_STATE = 'feedback-form-state';
-const formData = {};
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const formKey = "feedback-form-state";
 
-// Get form element
-const formRef = document.querySelector('.feedback-form');
+ let json = localStorage.getItem(formKey);
+let parsed = JSON.parse(json);
+ 
+form.addEventListener('input', onInput)
+form.addEventListener("submit", onSubmit)
 
-// Call function checks localStorage
-populateForm();
+defaultForm();
 
-// Add event listener on form submit and input
-formRef.addEventListener('submit', onFormSubmit);
-formRef.addEventListener('input', throttle(onTextInput, 500));
+console.log(parsed)
 
-// Write on formData value event listener input  and save in localStorag
-function onTextInput(e) {
-  const { name, value } = e.target;
 
-  formData[name] = value;
-  storageLocal.save(FB_FORM_STATE, formData);
+function onInput(){
+         json = localStorage.getItem(formKey);
+         parsed = JSON.parse(json);
+        console.log(parsed)
+    localStorage.setItem(formKey, JSON.stringify({
+       email : input.value,
+       message : textarea.value
+   }))
+}
+function onSubmit (evt){
+     evt.preventDefault();
+          json = localStorage.getItem(formKey);
+         parsed = JSON.parse(json)
+        console.log(parsed)
+    localStorage.clear();
+    input.value = '';
+    textarea.value = '';
+}
+function defaultForm(){
+         json = localStorage.getItem(formKey);
+         parsed = JSON.parse(json);
+console.log(parsed)
+    if(parsed){
+        input.value = parsed.email || '';
+        textarea.value = parsed.message || '';
+    }
 }
 
-// Checks localStorage and writ in form save value
-function populateForm() {
-  const savedFormData = storageLocal.load(FB_FORM_STATE);
 
-  if (savedFormData) {
-    const { email = '', message = '' } = savedFormData;
-    formRef.email.value = email;
-    formRef.message.value = message;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Import library
+// import throttle from 'lodash.throttle';
+// import * as storageLocal from './storage';
+
+// // Initial constants
+// const FB_FORM_STATE = 'feedback-form-state';
+// const formData = {};
+
+// // Get form element
+// const formRef = document.querySelector('.feedback-form');
+
+// // Call function checks localStorage
+// populateForm();
+
+// // Add event listener on form submit and input
+// formRef.addEventListener('submit', onFormSubmit);
+// formRef.addEventListener('input', throttle(onTextInput, 500));
+
+// // Write on formData value event listener input  and save in localStorag
+// function onTextInput(e) {
+//   const { name, value } = e.target;
+
+//   formData[name] = value;
+//   storageLocal.save(FB_FORM_STATE, formData);
+// }
+
+// // Checks localStorage and writ in form save value
+// function populateForm() {
+//   const savedFormData = storageLocal.load(FB_FORM_STATE);
+
+//   if (savedFormData) {
+//     const { email = '', message = '' } = savedFormData;
+//     formRef.email.value = email;
+//     formRef.message.value = message;
     
-    formData.email = email;
-    formData.message = message;
-  }
-}
+//     formData.email = email;
+//     formData.message = message;
+//   }
+// }
 
-// Clears the form and 'feedback-form-state' in localstorage
-function onFormSubmit(e) {
-  e.preventDefault();
+// // Clears the form and 'feedback-form-state' in localstorage
+// function onFormSubmit(e) {
+//   e.preventDefault();
 
-  console.log(formData);
+//   console.log(formData);
 
-  e.currentTarget.reset();
-  storageLocal.remove(FB_FORM_STATE);
+//   e.currentTarget.reset();
+//   storageLocal.remove(FB_FORM_STATE);
 
-  // Clears obj formData
-  for (const key in formData) {
-    if (formData.hasOwnProperty(key)) delete formData[key];
-  }
-}
+//   // Clears obj formData
+//   for (const key in formData) {
+//     if (formData.hasOwnProperty(key)) delete formData[key];
+//   }
+// }
 
 
 
