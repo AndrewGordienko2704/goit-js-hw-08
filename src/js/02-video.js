87304c36beb085876c0efbcbd529148d297eb679
AndrewGordienko2704 <109688447+AndrewGordienko2704@users.noreply.github.com>
2023-01-04 19:50:42 +0200

    hw--08

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,45 @@
-
 import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
+import * as storageLocal from './storage';
 
+// Constant value entry
+const VIDEOPLAYER_CURRENT_TIME = 'videoplayer-current-time';
+
+// Get videoplayer element and a player
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-const lastCurrentTime = localStorage.getItem('videoplayer-current-time');
+// Set and get current time from localStorage
+const onPlay = ({ seconds }) =>
+  storageLocal.save(VIDEOPLAYER_CURRENT_TIME, seconds);
+const videoplayerCurrentTime = storageLocal.load(VIDEOPLAYER_CURRENT_TIME) ?? 0;
 
-player.on('timeupdate', throttle(function ({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
-}, 1000));
+// Start traking the timeupdate event with a delay 1s and set current time
+player.on('timeupdate', throttle(onPlay, 1000));
+player.setCurrentTime(videoplayerCurrentTime);
 
-console.log(lastCurrentTime);
+
+
+
+
+
+
+
+
+
+// import throttle from 'lodash.throttle';
+// import Player from '@vimeo/player';
+
+// const iframe = document.querySelector('#vimeo-player');
+// const player = new Player(iframe);
+
+// const lastCurrentTime = localStorage.getItem('videoplayer-current-time');
+
+// player.on('timeupdate', throttle(function ({ seconds }) {
+//   localStorage.setItem('videoplayer-current-time', seconds);
+// }, 1000));
+
+// console.log(lastCurrentTime);
 
 
 
